63b66e8688fdf03c67a7d3fb728c3e5bba1e477b
PrayanshuChourasia <140251730+PrayanshuChourasia@users.noreply.github.com>
2024-04-19 05:55:24 +0530

    updated first file

---
+++ script.js
@@ -0,0 +1,11 @@
+function showsidebar(){
+    const sidebar=document.querySelector('.sidebar');
+    sidebar.style.display='flex'
+}
+function hidesidebar(){
+    const hidebar=document.querySelector('.sidebar');
+    hidebar.style.display='none'
+}
+function login(){
+    window.alert('THIS FEATURE IS COMING SOON .......')
+}
